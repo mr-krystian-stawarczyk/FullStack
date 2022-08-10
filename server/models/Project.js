f4ca98846ef410f1fe4239e2e850cb7012c3ef92
Krystian S <mr.krystian.stawarczyk@gmail.com>
2022-08-10 13:31:55 +0200

    Clients,Project queries

---
+++ server/models/Project.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ProjectSchema = new Schema({
+	name: {
+		type: String,
+	},
+	description: {
+		type: String,
+	},
+
+	status: {
+		type: String,
+		enum: ["Not Started", "In Progress", "Completed"],
+	},
+
+	clientId: { type: Schema.Types.ObjectId, ref: "Client" },
+});
+module.exports = mongoose.model("Project", ProjectSchema);
